03d8243da9adc1fc2881e63d3e688e8dbf4ef267
hase111111 <hasehasehase61@gmail.com>
2024-10-22 17:21:10 +0900

    ライセンスのコメントを追加

--- src/two_link/two_link_robot_param.py
+++ src/two_link/two_link_robot_param.py
@@ -1,4 +1,10 @@
 
+#-*- coding: utf-8 -*-
+
+# Copyright (c) 2024 Taisei Hasegawa
+# Released under the MIT license
+# https://opensource.org/licenses/mit-license.php
+
 import numpy as np
 
 from util import clamp_angle
