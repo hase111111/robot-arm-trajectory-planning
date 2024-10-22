2da6a1474523e087bb7886bc6fc4073e57a18900
hase111111 <hasehasehase61@gmail.com>
2024-10-22 19:19:14 +0900

    変数名の変更

--- src/two_link/two_link_robot.py
+++ src/two_link/two_link_robot.py
@@ -21,13 +21,13 @@ from two_link_robot_param import TwoLinkRobotParam, TwoLinkRobotColorParam
 class TwoLinkRobot:
     def __init__(self, param: TwoLinkRobotParam = TwoLinkRobotParam(), 
                  color_param: TwoLinkRobotColorParam = TwoLinkRobotColorParam()) -> None:
-        self._param = param
-        self._color_param = color_param
+        self.__param = param
+        self.__color_param = color_param
 
     def forward_kinematics(self, theta1: float, theta2: float) -> Tuple[float, float, float, float]:
-        l1 = self._param.link1
-        l2 = self._param.link2
-        o = self._param.origin
+        l1 = self.__param.link1
+        l2 = self.__param.link2
+        o = self.__param.origin
         x1 = o[0] + l1 * np.cos(theta1)
         y1 = o[1] + l1 * np.sin(theta1)
         x2 = x1 + l2 * np.cos(theta1 + theta2)
@@ -36,9 +36,9 @@ class TwoLinkRobot:
 
     def inverse_kinematics(self, end_effecter_x: float, end_effecter_y: float, *, other: bool = True) -> Tuple[float, float]:
         # 見やすくするために変数名を短くしている
-        l1:float = self._param.link1
-        l2:float = self._param.link2
-        o = self._param.origin
+        l1:float = self.__param.link1
+        l2:float = self.__param.link2
+        o = self.__param.origin
         x:float = end_effecter_x - o[0]
         y:float = end_effecter_y - o[1]
 
@@ -56,22 +56,22 @@ class TwoLinkRobot:
         return theta1, theta2
     
     def theta1_is_in_range(self, theta1: float) -> bool:
-        return self._param.theta1_min <= theta1 <= self._param.theta1_max
+        return self.__param.theta1_min <= theta1 <= self.__param.theta1_max
     
     def theta2_is_in_range(self, theta2: float) -> bool:
-        return self._param.theta2_min <= theta2 <= self._param.theta2_max
+        return self.__param.theta2_min <= theta2 <= self.__param.theta2_max
     
     def is_in_range(self, theta1: float, theta2: float) -> bool:
         return self.theta1_is_in_range(theta1) and self.theta2_is_in_range(theta2)
     
     def plot(self, ax:axes.Axes, theta1: float, theta2: float) -> None:
         x1, y1, x2, y2 = self.forward_kinematics(theta1, theta2)
-        o = self._param.origin
-        ax.plot([o[0], x1], [o[1], y1], color=self._color_param.link1_color, linewidth=self._color_param.link_width, zorder=1)
-        ax.plot([x1, x2], [y1, y2], color=self._color_param.link2_color, linewidth=self._color_param.link_width, zorder=1)
-        ax.add_patch(Circle(o, self._color_param.joint_size, color=self._color_param.origin_color, zorder=2))
-        ax.add_patch(Circle([x1, y1], self._color_param.joint_size, color=self._color_param.joint1_color, zorder=2))
-        ax.add_patch(Circle([x2, y2], self._color_param.joint_size, color=self._color_param.joint2_color, zorder=2))
+        o = self.__param.origin
+        ax.plot([o[0], x1], [o[1], y1], color=self.__color_param.link1_color, linewidth=self.__color_param.link_width, zorder=1)
+        ax.plot([x1, x2], [y1, y2], color=self.__color_param.link2_color, linewidth=self.__color_param.link_width, zorder=1)
+        ax.add_patch(Circle(o, self.__color_param.joint_size, color=self.__color_param.origin_color, zorder=2))
+        ax.add_patch(Circle([x1, y1], self.__color_param.joint_size, color=self.__color_param.joint1_color, zorder=2))
+        ax.add_patch(Circle([x2, y2], self.__color_param.joint_size, color=self.__color_param.joint2_color, zorder=2))
 
 
 def main() -> None:
